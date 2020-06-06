c58562a8cf9234a1f2c5b73201d5b5c440f36295
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-06-06 11:27:43 -0300

    "Se creo Ventana Conexion"

--- TareaConexionODBC/Form1.Designer.vb
+++ TareaConexionODBC/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class formPpal
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,10 +22,44 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.BackColor = System.Drawing.Color.White
+        Me.btnConnect.Location = New System.Drawing.Point(112, 86)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(135, 48)
+        Me.btnConnect.TabIndex = 0
+        Me.btnConnect.Text = "Connect"
+        Me.btnConnect.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(128, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Comprobar Conexion"
+        '
+        'formPpal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(375, 196)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Name = "formPpal"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents Label1 As Label
 End Class
--- TareaConexionODBC/Form1.vb
+++ TareaConexionODBC/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Public Class formPpal
+    Private Sub formPpal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+
+    End Sub
 End Class
