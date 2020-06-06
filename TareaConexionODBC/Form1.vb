a61033ff234ffb26a98f07be98f2685d7c3f9093
Marcos Capillera <62310940+marcoscapillera@users.noreply.github.com>
2020-06-06 18:20:31 -0300

    "Se crea insert"

--- TareaConexionODBC/Form1.vb
+++ TareaConexionODBC/Form1.vb
@@ -1,4 +1,7 @@
-﻿Public Class formPpal
+﻿Imports System.Data.Odbc
+
+
+Public Class formPpal
     Private Sub formPpal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
@@ -8,6 +11,22 @@
     End Sub
 
     Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        Dim conexion As New OdbcConnection("DRIVER=MySQL ODBC 5.3 ANSI Driver;UID=marcos;PWD=admin;PORT=3306;DATABASE=testconexion;SERVER=localhost")
+
+        conexion.Open()
+        MsgBox("Conexion Exitosa!")
+
+
+        Dim comando As New OdbcCommand
+        comando.CommandText = "INSERT INTO persona (id, nombre, apellido) VALUES (1,'Juan','Perez')"
+        comando.Connection = conexion
+
+        comando.ExecuteNonQuery()
+
+        MsgBox("Persona insert correct")
+
 
     End Sub
 End Class
+
+
